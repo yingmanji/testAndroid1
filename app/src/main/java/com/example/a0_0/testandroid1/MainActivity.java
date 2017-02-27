d65da95200050c045cd6c26603842bd596a6807a
樱满集0_0 <2283438112@qq.com>
2017-02-27 13:11:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a0_0/testandroid1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a0_0.testandroid1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
